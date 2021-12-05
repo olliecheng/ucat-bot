e30e2e6a2f17a6836f719eb132e38b2e8f2edb59
Ollie Cheng <denosawr@vivaldi.net>
2021-12-05 13:48:37 +0800

    Prefill a unique ID in the form

--- src/modules/submit-results.ts
+++ src/modules/submit-results.ts
@@ -12,13 +12,27 @@ import { Module, Command, Event } from "../interfaces";
 
 import SparkMD5 from "spark-md5";
 
+enum Region {
+  UK = "UK",
+  ANZ = "ANZ",
+}
+
+function generatePrefilledLink(user_id: string, region: Region) {
+  // https://docs.google.com/forms/d/e/1FAIpQLSdIZOR746q0qHXBj9ucPARcoq_rAiUWCCD0pKkfw7HBY_4F5g/
+  //    viewform?usp=pp_url&entry.475796844=ABC&entry.2013655943=UK
+
+  return (
+    "https://docs.google.com/forms/d/e/1FAIpQLSdIZOR746q0qHXBj9ucPARcoq_rAiUWCCD0pKkfw7HBY_4F5g/" +
+    `viewform?usp=pp_url&entry.475796844=${user_id}&entry.2013655943=${region}`
+  );
+}
+
 const submitMockResults: Command = {
   name: "submit-mock-results",
   description:
     "Submit your mock results to the database to gauge your performance.",
 
   handler: async (client, interaction) => {
-    const userID = SparkMD5.hash(interaction.user.id);
     const row = new MessageActionRow().addComponents(
       new MessageSelectMenu()
         .setCustomId("provider")
@@ -47,6 +61,11 @@ const interactionChangedEvent: Event = {
       return;
     }
 
+    // base64 encode of the MD5 hash of the Discord username
+    const userID = Buffer.from(
+      SparkMD5.hash(genericInteraction.user.id)
+    ).toString("base64");
+
     const interaction = genericInteraction as SelectMenuInteraction;
     const provider: string = interaction.values[0];
 
@@ -62,7 +81,7 @@ const interactionChangedEvent: Event = {
     );
     const URLRow = new MessageActionRow().addComponents(
       new MessageButton()
-        .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        .setURL(generatePrefilledLink(userID, Region.ANZ))
         .setStyle("LINK")
         .setLabel(`Link to submit ${provider} results`)
         .setEmoji("📎")
